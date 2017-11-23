5bb86ebe6b52087b2a106d5afefc65a7c1a86e81
Damien Rochat <rochat.damien@gmail.com>
2017-11-23 20:12:35 +0100

    serialize route coordinates

--- biketracks/routes/serializers.py
+++ biketracks/routes/serializers.py
@@ -1,10 +1,20 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.fields import FloatField
+from rest_framework.serializers import ModelSerializer, Serializer
 
 from routes.models import Route
 
 
+class RoutePointSerializer(Serializer):
+    lat = FloatField(required=True)
+    lng = FloatField(required=True)
+
+    def to_representation(self, instance):
+        return dict(lat=instance[1], lng=instance[0])
+
+
 class RouteSerializer(ModelSerializer):
+    route = RoutePointSerializer(many=True)
 
     class Meta:
         model = Route
-        fields = ('id', 'name', 'route')
+        fields = '__all__'
