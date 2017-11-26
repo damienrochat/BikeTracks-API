63d267d82d7e1692a958e860ae1eba30d34c163d
Damien Rochat <rochat.damien@gmail.com>
2017-11-26 22:08:06 +0100

    store distance and descent/climb

--- biketracks/routes/management/commands/importgpx.py
+++ biketracks/routes/management/commands/importgpx.py
@@ -1,4 +1,5 @@
 import gpxpy
+from django.contrib.gis.gdal import CoordTransform, SpatialReference
 from django.contrib.gis.geos import Point, LineString
 from django.core.management import BaseCommand
 
@@ -6,22 +7,39 @@ from routes.models import Route
 
 
 class Command(BaseCommand):
-    help = 'Import all GPX files from data directory'
+    help = 'Import GPX files'
 
     def add_arguments(self, parser):
         parser.add_argument('files', nargs='+', type=str)
 
     def handle(self, *args, **options):
+        ct = CoordTransform(SpatialReference(4326), SpatialReference(3035))  # transform wgs84 to european grid
+
         for filename in options['files']:
             gpx = gpxpy.parse(open(filename))
 
             for track in gpx.tracks:
                 for segment in track.segments:
+                    climb = 0
+                    descent = 0
+
+                    for i in range(1, len(segment.points)):
+                        delta = segment.points[i].elevation - segment.points[i-1].elevation
+                        if delta > 0:
+                            climb = climb + delta
+                        elif delta < 0:
+                            descent = descent + delta
+
+                    route = LineString([
+                        Point(x=point.longitude, y=point.latitude, z=point.elevation)
+                        for point in segment.points
+                    ], srid=4326)
+
                     Route.objects.create(
                         name=track.name,
                         type=track.type,
-                        route=LineString([
-                            Point(x=point.longitude, y=point.latitude, z=point.elevation)
-                            for point in segment.points
-                        ], srid=4326)
+                        route=route,
+                        distance=route.transform(ct, clone=True).length,  # distance based on ETRS89 grid
+                        climb=climb,
+                        descent=descent,
                     )
--- biketracks/routes/models.py
+++ biketracks/routes/models.py
@@ -9,21 +9,19 @@ class RouteQuerySet(GeoQuerySet):
         """
         QuerySet of all routes in the area,
         defined by the geo point and the radius (in meters).
-
-        Add and order by 'distance' attribute.
         """
-        return self.filter(route__distance_lte=(point, D(m=radius))).distance(point).order_by('distance')
+        return self.filter(route__distance_lte=(point, D(m=radius)))
 
 
 class Route(models.Model):
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
     route = models.LineStringField(dim=3, srid=4326)
+    distance = models.IntegerField()
+    climb = models.IntegerField()
+    descent = models.IntegerField()
 
     objects = RouteQuerySet.as_manager()
 
     def __str__(self):
         return self.name
-
-    class Meta:
-        db_table = 'route'
